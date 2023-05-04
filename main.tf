f60be273e50859cdfa35397cda97ff351fc20bc0
BassaBN <130400884+BassaBN@users.noreply.github.com>
2023-05-05 04:23:20 +0800

    TTTT

--- main.tf
+++ main.tf
@@ -9,8 +9,16 @@ data "terraform_remote_state" "network" {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 4.0"
   region  = data.terraform_remote_state.network.outputs.aws_region
 }
 
